08c1645ee80d17f3c894e4cd5ba3f9e41f0baced
Gipnokot <shootinputin007@gmail.com>
2025-02-06 17:07:59 +0500

    Add application_helper_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get  "static_pages/home"
-  get  "static_pages/help"
-  get  "static_pages/about"
-  get "static_pages/contact"
-
-
-
-
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,14 @@
+# test/helpers/application_helper_test.rb
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    # Проверка заголовка без аргумента
+    assert_equal "Ruby on Rails Tutorial Sample App", full_title
+
+    # Проверка заголовка с аргументом
+    assert_equal "Help | Ruby on Rails Tutorial Sample App", full_title("Help")
+    assert_equal "About | Ruby on Rails Tutorial Sample App", full_title("About")
+    assert_equal "Contact | Ruby on Rails Tutorial Sample App", full_title("Contact")
+  end
+end

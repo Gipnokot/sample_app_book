ad0e683aeb38e1b781abdcc973700de37298ea71
Gipnokot <shootinputin007@gmail.com>
2025-04-03 18:53:48 +0500

    fix action mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "sample-app-book.onrender.com", protocol: "https" }
 
   ActionMailer::Base.smtp_settings = {
     address: "smtp.gmail.com",
